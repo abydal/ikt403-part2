fbd3e72dd7c73e76bfba0e109f5d90612d2b39af
asbjorn.bydal <asbjorn.bydal@c0a7331c-bb3b-0410-a17d-c7c670e4c10f>
2007-10-12 15:15:14 +0000

    Added - Files TestStructure.java and TestBlogPostImpl.java Updated - a bunch of other files

---
+++ trunk/socialnetwork/TestBlogPostImpl.java
@@ -0,0 +1,69 @@
+package socialnetwork;
+
+import junit.framework.TestCase;
+
+public class TestBlogPostImpl extends TestCase {
+
+	BlogPostImpl blogPost;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		blogPost = new BlogPostImpl();
+		
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	
+		blogPost = null;
+	}
+
+	public void testAddComment() {
+		CommentImpl theComment = new CommentImpl();
+		
+		blogPost.addComment(theComment);
+
+		assertTrue(blogPost.getComment().get(0) == theComment);
+		//fail("Not yet implemented");
+	}
+
+	public void testRemoveComment() {
+
+	// First add
+		CommentImpl theComment = new CommentImpl();
+		blogPost.addComment(theComment);
+		assertTrue(blogPost.getComment().get(0) == theComment);
+
+	// Then remove
+		blogPost.removeComment(theComment);
+
+		assertTrue(blogPost.getComment().size() == 0);
+		
+	}
+
+	public void testAddPicture() {
+
+		PictureImpl thePicture = new PictureImpl();
+		
+		blogPost.addPicture(thePicture);
+
+		assertTrue(blogPost.getPicture().get(0) == thePicture);
+	}
+
+	public void testRemovePicture() {
+		
+	// First add
+		PictureImpl thePicture = new PictureImpl();
+		blogPost.addPicture(thePicture);
+		assertTrue(blogPost.getPicture().get(0) == thePicture);
+
+	// Then remove
+		blogPost.removePicture(thePicture);
+
+		assertTrue(blogPost.getPicture().size() == 0);
+		
+
+	}
+
+}
